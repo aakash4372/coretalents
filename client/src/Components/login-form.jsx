201f5f9ed8ba39abb5c2e937dd22d43019c2a5a4
aakash4372 <aakashaakash0013@gmail.com>
2025-11-23 17:44:26 +0530

    fadf

--- client/src/Components/login-form.jsx
+++ client/src/Components/login-form.jsx
@@ -5,8 +5,8 @@ import {
   CardContent,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
+} from "@/Components/ui/card";
+import { Input } from "@/Components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
